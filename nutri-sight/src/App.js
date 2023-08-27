1e73086bb9e9fc98bc72810516fc19c70192723e
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-27 14:58:40 -0400

    Adjusting routing

--- nutri-sight/src/App.js
+++ nutri-sight/src/App.js
@@ -1,11 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomeRoute from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import NutritionTracker from "./routes/nutrition-tracker/nutrition-tracker.component";
+import Store from "./routes/store/store.component";
+import AuthenticationRoute from "./routes/authentication/authentication.component";
 
 function App() {
   return (
-    <div>
-      <h1>nutri-sight</h1>
-      <h1>Login</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Navigation/> }>
+        <Route index element={ <HomeRoute/> }/>
+        <Route path="nutrition-tracker" element={ <NutritionTracker/> }/>
+        <Route path="store" index element={ <Store/> }/>
+        <Route path="auth" index element={ <AuthenticationRoute/> }/>
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
