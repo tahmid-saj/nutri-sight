1edfa5fff2639d70830b685147e34dd17bcb7322
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-08-27 15:36:04 -0400

    Delete food-classifier directory

--- food-classifier/src/ts/model_evaluation/model_evaluation.py
+++
@@ -1,9 +0,0 @@
-def get_ensemble_models_summary(models):
-    """
-    Generates model summaries of ensemble models.
-    Parameters
-    ----------
-    models: Ensemble of models.
-    """
-    for model in models:
-        print(model.summary())
