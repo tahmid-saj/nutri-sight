d9a7524cd92befb6fdc8eb46e9c116dfc43aee0f
tahmid-saj <tahmid.saj@gmail.com>
2023-10-12 16:38:16 -0400

    adding view class

---
+++ nutri-sight/src/components/shared/recipes/static/js/views/view.js
@@ -0,0 +1,94 @@
+import icons from "../../img/icons.svg";
+
+class View {
+  _data;
+
+  render(data, render = true) {
+    if (!data || (Array.isArray(data) && data.length === 0)) {
+      return this.renderError();
+    }
+
+    this._data = data;
+    const markup = this._generateMarkup();
+
+    if (!render) return markup;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  };
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll("*"));
+    const curElements = Array.from(this._parentElement.querySelectorAll("*"));
+
+    newElements.forEach((newElement, i) => {
+      const curElement = curElements[i];
+
+      // updates changed text
+      if (!newElement.isEqualNode(curElement) && newElement.firstChild?.nodeValue.trim() !== '') {
+        curElement.textContent = newElement.textContent;
+      }
+
+      // updates changed attributes
+      if (!newElement.isEqualNode(curElement)) {
+        Array.from(newElement.attributes).forEach(attribute => curElement.setAttribute(attribute.name, attribute.value));
+      }
+    });
+  };
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
+
+     this._clear();
+     this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  };
+
+  renderError(message = this._message) {
+    const markup = `
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  };
+
+  renderMessage(message = this._message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+};
+
+export default View;
