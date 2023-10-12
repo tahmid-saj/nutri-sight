fa65382b6dfd03c4b13656dfac5fc36c1d032e2a
tahmid-saj <tahmid.saj@gmail.com>
2023-10-12 19:59:12 -0400

    updating template

---
+++ nutri-sight/src/components/shared/recipes/recipes.component.jsx
@@ -0,0 +1,7 @@
+import React, { useRef, useEffect } from "react";
+import ReactDom from "react-dom/client";
+
+export default function Recipes() {
+  const container = document.getElementById("container");
+  const root = ReactDOM.createRoot(container);
+};
