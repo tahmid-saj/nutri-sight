61d7bee32e63b7fad34af5029ad8d131c043d5bb
tahmid-saj <tahmid.saj@gmail.com>
2023-10-13 19:04:38 -0400

    recipe ingredients

--- nutri-sight/src/components/shared/recipes/components/recipe/ingredients/ingredients.component.jsx
+++ nutri-sight/src/components/shared/recipes/components/recipe/ingredients/ingredients.component.jsx
@@ -1,15 +1,22 @@
 import "./ingredients.styles.scss";
 
 const Ingredients = ({ currentDisplayedRecipe }) => {
+  const { ingredients } = currentDisplayedRecipe;
+
   return (
     <div className="ingredients-container">
       <h2>Recipe Ingredients</h2>
 
       <div className="ingredient-quantities-container">
-        <h4>{`${"3 cups bread flour"}`}</h4>
-        <h4>{`${"3 cups bread flour"}`}</h4>
-        <h4>{`${"3 cups bread flour"}`}</h4>
-        <h4>{`${"3 cups bread flour"}`}</h4>
+        {
+          ingredients.map((ingredient, index) => {
+            return (
+              <h4 key={ index }>{`${ingredient.quantity !== null ? ingredient.quantity : ""} ${ingredient.unit} ${ingredient.description}`}</h4>
+            )
+          })
+        }
+
+
       </div>
     </div>
   )
