89265996bfce11a7674c2d6d879ba61289a7e587
tahmid-saj <tahmid.saj@gmail.com>
2023-10-14 04:53:43 -0400

    components for nutrient predictor

--- nutri-sight/src/components/shared/nutrient-predictor/nutrient-predictor.component.jsx
+++ nutri-sight/src/components/shared/nutrient-predictor/nutrient-predictor.component.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./nutrient-predictor.styles.scss";
 import UploadFoodImage from "./upload-food-image/upload-food-image.component";
 import NutrientsInfo from "./nutrients-info/nutrients-info.component";
 
-const NutrientPredictor = () => {
-  return (
-    <div className="upload-food-image-nutrients-info-container">
-      <UploadFoodImage></UploadFoodImage>
+class NutrientPredictor extends Component {
+  constructor() {
+    super();
 
-      <NutrientsInfo></NutrientsInfo>
-    </div>
-  )
+    this.state = {
+      displayNutrientsInfo: false,
+    }
+  };
+
+  displayNutrients = (displayNutrientsInfo) => {
+    this.setState({ displayNutrientsInfo: displayNutrientsInfo })
+  }
+
+  render = () => {
+    return (
+      <div className="upload-food-image-nutrients-info-container">
+        <UploadFoodImage displayNutrients={ this.displayNutrients }></UploadFoodImage>
+  
+        {
+          this.state.displayNutrientsInfo &&
+          <NutrientsInfo></NutrientsInfo>
+        }
+      </div>
+    )
+  }
+
 };
 
 export default NutrientPredictor;
--- nutri-sight/src/components/shared/nutrient-predictor/upload-food-image/upload-food-image.component.jsx
+++ nutri-sight/src/components/shared/nutrient-predictor/upload-food-image/upload-food-image.component.jsx
@@ -4,10 +4,10 @@ import "./upload-food-image.styles.scss";
 import UploadImage from "./upload-image/upload-image.component";
 import DetectNutrients from "./detect-nutrients/detect-nutrients.component";
 
-const UploadFoodImage = () => {
+const UploadFoodImage = ({ displayNutrients }) => {
   return (
     <div className="upload-image-detect-nutrients-container">
-      <UploadImage></UploadImage>
+      <UploadImage displayNutrients={ displayNutrients }></UploadImage>
       <DetectNutrients></DetectNutrients>
     </div>
   )
