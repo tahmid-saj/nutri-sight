7e55aee37c082adb4a04f0f0d0221b05251a4941
tahmid-saj <tahmid.saj@gmail.com>
2023-10-18 21:09:40 -0400

    nutrient predictor context

--- nutri-sight/src/components/shared/nutrient-predictor/upload-food-image/upload-image/upload-image.component.jsx
+++ nutri-sight/src/components/shared/nutrient-predictor/upload-food-image/upload-image/upload-image.component.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import "./upload-image.styles.scss";
 import FormInput from "../../../form-input/form-input.component";
 import Button from "../../../button/button.component";
 
+import { NutrientPredictorContext } from "../../../../../context/shared/nutrient-predictor/nutrient-predictor.context";
+
 const UploadImage = ({ displayNutrients }) => {
-  const [uploadedImage, setUploadedImage] = useState("");
+  const [uploadedImage, setUploadedImage] = useState();
+
+  const { imageAndPrediction, updateImage, updateImageAndPrediction } = useContext(NutrientPredictorContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     console.log(uploadedImage);
 
+    updateImageAndPrediction(uploadedImage, "image");
     displayNutrients(true);
   };
 
   const handleChange = (event) => {
     event.preventDefault();
 
+    console.log(event.target.value);
+
     setUploadedImage(event.target.value);
+    updateImage(event.target.value);
   };
 
   return (
@@ -27,7 +35,7 @@ const UploadImage = ({ displayNutrients }) => {
 
       <form onSubmit={ handleSubmit }>
         <FormInput type="file" id="uploadedImage" name="uploadedImage" 
-                  onChange={ handleChange } required value={ uploadedImage }></FormInput>
+                  onChange={ handleChange } required value={ uploadedImage } accept="image/*"></FormInput>
         <Button buttonType="regularButton" type="submit">Detect Nutrients</Button>
       </form>
     </div>
