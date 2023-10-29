63f4e66ae48a43da2fe1e74dd0c36ff5a6ba1e97
tahmid-saj <tahmid.saj@gmail.com>
2023-10-28 21:13:11 -0400

    nutrition tracker chart for signed in

--- nutri-sight/src/components/signed-in/nutrition-tracker/top-search/top-search.component.jsx
+++ nutri-sight/src/components/signed-in/nutrition-tracker/top-search/top-search.component.jsx
@@ -5,11 +5,13 @@ import "./top-search.styles.scss";
 import SearchDays from "../search-days/search-days.component";
 
 import Summary from "../summary/summary.component";
+import SummaryGraph from "../summary-graph/summary-graph.component";
   
 const TopSearch = () => {
   return (
     <div className="search-days-summary-container">
       <Summary></Summary>
+      <SummaryGraph></SummaryGraph>
 
       <div className="search-days-nutrition-tracker-container">
         <SearchDays></SearchDays>
