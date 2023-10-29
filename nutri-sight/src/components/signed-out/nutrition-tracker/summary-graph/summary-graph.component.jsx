1410fdb69f6ec0b9e7bc2cc95a48193eac92902c
tahmid-saj <tahmid.saj@gmail.com>
2023-10-28 21:15:15 -0400

    nutrition tracker chart for signed in

--- nutri-sight/src/components/signed-out/nutrition-tracker/summary-graph/summary-graph.component.jsx
+++ nutri-sight/src/components/signed-out/nutrition-tracker/summary-graph/summary-graph.component.jsx
@@ -92,7 +92,7 @@ const SummaryGraph = () => {
 
   return (
     <Fragment>
-      <h2>Consumption Summary</h2>
+      <h2>Consumption Timeline</h2>
       <div className="summary-graph-container">
         <ReactApexChart options={ options } series={ series } type="bar" height={ 350 } width={ 1000 }/>
       </div>
