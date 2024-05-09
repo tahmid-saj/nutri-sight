05f249fd97ac455633216e3a024f79842f2c15fa
tahmid-saj <tahmid.saj@gmail.com>
2024-05-08 22:01:30 -0400

    calories burned files

---
+++ nutri-sight/src/routes/signed-in/calories-burned/calories-burned.component.jsx
@@ -0,0 +1,9 @@
+import "./calories-burned.styles.scss";
+
+const CaloriesBurnedRouteSignedIn = () => {
+  return (
+    <h1>Calories Burned</h1>
+  );
+};
+
+export default CaloriesBurnedRouteSignedIn;
---
+++ nutri-sight/src/routes/signed-out/calories-burned/calories-burned.component.jsx
@@ -0,0 +1,11 @@
+import "./calories-burned.styles.scss";
+
+import CaloriesBurned from "../../../components/signed-out/calories-burned/calories-burned.component";
+
+const CaloriesBurnedRoute = () => {
+  return (
+    <CaloriesBurned></CaloriesBurned>
+  );
+};
+
+export default CaloriesBurnedRoute;
