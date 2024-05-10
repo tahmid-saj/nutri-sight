cc1927241148492b4dee65dad1246a7644c9ede5
tahmid-saj <tahmid.saj@gmail.com>
2024-05-10 00:15:42 -0400

    nutrition pred requests

--- nutri-sight/src/components/shared/nutrient-predictor/nutrients-info/nutrients-info.component.jsx
+++ nutri-sight/src/components/shared/nutrient-predictor/nutrients-info/nutrients-info.component.jsx
@@ -1,31 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment } from "react";
 
 import "./nutrients-info.styles.scss";
 
 import { NutrientPredictorContext } from "../../../../contexts/shared/nutrient-predictor/nutrient-predictor.context";
 
 const NutrientsInfo = () => {
-  const { prediction } = useContext(NutrientPredictorContext).imageAndPrediction;
+  const { nutrientPredictions } = useContext(NutrientPredictorContext);
 
   return (
-    <div className="nutrients-info-container">
-      <strong><h1>{`${prediction.food}`}</h1></strong>
+    nutrientPredictions.map((prediction, index) => {
+      return (
+        <div key={ index } className="nutrients-info-container">
+          <strong><h1>{`${prediction.name}`}</h1></strong>
+          <h4>{`Calories - ${prediction.calories}`}</h4>
+          <h5>{`Serving size - ${prediction.servingSizeG} g`}</h5>
+          <br></br>
+          <strong><h2>Macronutrients</h2></strong>
+          <h4>{`Carbohydrates - ${prediction.macronutrients.carbohydratesTotalG} g`}</h4>
+          <h4>{`Protein - ${prediction.macronutrients.proteinG} g`}</h4>
+          <h4>{`Fat - ${prediction.macronutrients.fatTotalG} g`}</h4>
+          <h4>{`Saturated fat - ${prediction.macronutrients.fatSaturatedG} g`}</h4>
 
-      <strong><h2>Macronutrients</h2></strong>
-      <h4>{`Calories - ${prediction.calories}`}</h4>
-      <h4>{`Carbohydrates - ${prediction.macronutrients.carbohydrates.amount} ${prediction.macronutrients.carbohydrates.unit}`}</h4>
-      <h4>{`Protein - ${prediction.macronutrients.protein.amount} ${prediction.macronutrients.protein.unit}`}</h4>
-      <h4>{`Fat - ${prediction.macronutrients.fat.amount} ${prediction.macronutrients.fat.unit}`}</h4>
+          <strong><h2>Micronutrients</h2></strong>
+          {
+            prediction.micronutrients.map((micronutrient, index) => {
+              return (
+                <Fragment key={ index }>
+                  <h4>{`Sodium - ${micronutrient.sodiumMG} mg`}</h4>
+                  <h4>{`Potassium - ${micronutrient.potassiumMG} mg`}</h4>
+                  <h4>{`Cholesterol - ${micronutrient.cholesterolMg} mg`}</h4>
+                  <h4>{`Fiber - ${micronutrient.fiberG} g`}</h4>
+                  <h4>{`Sugar - ${micronutrient.sugarG} g`}</h4>
+                </Fragment>
+              )
+            })
+          }
 
-      <strong><h2>Micronutrients</h2></strong>
-
-      {
-        prediction.micronutrients.map((micronutrient, index) => {
-          return <h4 key={index}>{`${micronutrient.name} - ${micronutrient.amount} ${micronutrient.unit}`}</h4>
-        })
-      }
-
-    </div>
+        </div>
+      )
+    })
   )
 };
 
