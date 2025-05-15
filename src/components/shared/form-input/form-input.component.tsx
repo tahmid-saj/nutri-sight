bc1456e6525b33df7e88b7807111845c9eec6b69
tahmid-saj <tahmid.saj@gmail.com>
2025-05-15 02:33:43 -0400

    pages / shared components ts updates

---
+++ src/components/shared/form-input/form-input.component.tsx
@@ -0,0 +1,27 @@
+import "./form-input.styles.tsx";
+import { FormInputLabel, Input, Group } from "./form-input.styles.tsx";
+import React, { FC } from 'react';
+
+interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  name: string;
+  value: string;
+}
+
+const FormInput = ({ label, ...otherProps }: FormInputProps) => {
+  return (
+    <Group>
+      <Input { ...otherProps }/>
+
+      {
+        label && (
+          <FormInputLabel>
+            { label }
+          </FormInputLabel>
+        )
+      }
+    </Group>
+  );
+};
+
+export default FormInput;
